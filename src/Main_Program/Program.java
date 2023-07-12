eebf9951d1ca46aeb25db73b0c68b77ee558625f
cosmix768 <78139818+cosmix768@users.noreply.github.com>
2023-07-12 11:09:08 +0700

    Add files via upload

---
+++ src/Main_Program/Program.java
@@ -0,0 +1,9 @@
+package Main_Program;
+import Form.Form_Login;
+
+public class Program {
+    public static void main(String[] args){
+        Form_Login login = new Form_Login();
+        login.setVisible(true);
+    }
+}
